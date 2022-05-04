1cbff7ccf3df2767ae8f542ce091eb39c3df5004
matildesequeira <up202007623@edu.fe.up.pt>
2022-05-04 18:08:20 +0100

    Created classes Restaurant and Dish

--- database/restaurant.class.php
+++ database/restaurant.class.php
@@ -3,12 +3,12 @@
 
   class Restaurant {
     public int $id;
-    public string $idUser;
+    public int $idUser;
     public string $name;
     public string $address;
     public string $image;
 
-    public function __construct(int $id, string $idUser, string $name, string $address, string $image)
+    public function __construct(int $id, int $idUser, string $name, string $address, string $image)
     {
       $this->id = $id;
       $this->idUser=$idUser;
@@ -25,6 +25,31 @@
       $stmt->execute();
       return $stmt->fetchAll();;
     }
-  
+
+      static function getFavoriteRestaurants(PDO $db) : array {
+          $stmt = $db->prepare('
+        SELECT *
+        FROM FavoriteRestaurant
+      ');
+          $stmt->execute();
+          return $stmt->fetchAll();;
+      }
+
+
+      static function getRestaurant(PDO $db, int $id) : Restaurant {
+          $stmt = $db->prepare('SELECT *  FROM Restaurant WHERE id = ?');
+          $stmt->execute(array($id));
+
+          $restaurant = $stmt->fetch();
+
+          return new Restaurant(
+              $restaurant['id'],
+              $restaurant['idUser'],
+              $restaurant['name'],
+              $restaurant['address'],
+              $restaurant['image']
+          );
+      }
+
   }
 ?>
