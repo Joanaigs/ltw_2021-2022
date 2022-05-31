6b488e461d9db7bb672eb541991de32ff2835ee7
Joana <up202006279@edu.fe.up.pt>
2022-05-31 01:19:01 +0100

    comments done

---
+++ addReview.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+require_once('session.php');
+$session = new Session();
+require_once('database/restaurant.class.php');
+require_once('templates/common.tpl.php');
+require_once('templates/comments.tpl.php');
+require_once('database/user.class.php');
+require_once('database/review.class.php');
+require_once ('database/connection.db.php');
+
+$db = getDatabaseConnection();
+$idRestaurant=$_GET['idRestaurant'];
+
+if (isset($_POST["rate"], $_POST["remark"])){
+    $date=date("d-m-Y");
+    Review::addReview($db, $idRestaurant, $session->getId() , $_POST["remark"], $date, intval($_POST["rate"]));
+    header("Location: restaurant.php?id=$idRestaurant");
+}
