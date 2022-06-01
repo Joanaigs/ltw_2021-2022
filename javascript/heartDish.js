50a2ed4b24e05ff6e74970e788bba9a5a778df54
Joana <up202006279@edu.fe.up.pt>
2022-06-01 10:32:41 +0100

    fixed small problem

--- javascript/heartDish.js
+++ javascript/heartDish.js
@@ -1,11 +1,12 @@
 function heartsDishClick(){
-    const button = document.querySelectorAll('#dishes .heart');
+    const button = document.querySelectorAll('.hearts .heart');
     console.log(button)
     for(let i=0; i<button.length; i++) {
         button[i].addEventListener('click', async function () {
             if (button[i].classList.contains("liked")) {
                 button[i].classList.remove("liked");
                 const response = await fetch('../api/api_favoriteDish.php?add='+false +'&' + 'id='+this.id)
+                console.log(button)
                 await response.json()
             } else {
                 button[i].classList.add("liked");
