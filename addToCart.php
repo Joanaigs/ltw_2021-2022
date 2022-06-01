1419f791052ec22ac618d245cd1e5466eed3def4
marianaosiecka <mari.osiecka.carvalho@gmail.com>
2022-06-01 23:13:39 +0100

    fixed css for latest orders page

--- addToCart.php
+++ addToCart.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 require_once('session.php');
 $session = new Session();
 require_once('database/restaurant.class.php');
-require_once ('database/cart.class.php');
+require_once('database/cart.class.php');
 require_once('templates/common.tpl.php');
 require_once('templates/filter.tpl.php');
 require_once('templates/restaurants.tpl.php');
