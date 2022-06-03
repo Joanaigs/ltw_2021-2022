b44bb47604195a4829c8e23aeafe662419de9424
Joana <up202006279@edu.fe.up.pt>
2022-06-03 01:46:53 +0100

    fixed total price

--- templates/cart.tpl.php
+++ templates/cart.tpl.php
@@ -19,12 +19,12 @@
                             <div class="orders">
                             <?php foreach ($cart as $dish) { ?>
                                 <article id="item">
-                                    <?php $total += $dish->price?>
+                                    <?php $total += $dish->price*$dish->number?>
                                     <div class="name-price">
                                         <a href="../restaurant.php?id=<?=$dish->idRestaurant?>#<?=$dish->name?>"><?=$dish->name?> x<?=$dish->number?></a>
                                         <a href="../updadeCartNumber.php?idDish=<?= $dish->id ?>&number=<?=$dish->number+1?>&idRestaurant=<?=$dish->idRestaurant?>" class="fa-solid fa-plus"></a>
                                         <a href="../updadeCartNumber.php?idDish=<?= $dish->id ?>&number=<?=$dish->number-1?>&idRestaurant=<?=$dish->idRestaurant?>" class="fa fa-minus"></a>
-                                        <p><?=$dish->price?> €</p>
+                                        <p><?=$dish->price*$dish->number?> €</p>
                                     </div>
                                     <button class="erase-fromCart-btn" name="eraseFromButton" onclick="window.location.href = '../removeFromCart.php?idRestaurant=<?=$dish->idRestaurant?>&idDish=<?=$dish->id?>&cart=true';"> Remover</button>
                                 </article>
