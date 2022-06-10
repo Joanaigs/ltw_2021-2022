9cb6c5cbd2fd636b13f3d354ce228ac9cc51c814
Joana <up202006279@edu.fe.up.pt>
2022-06-10 17:51:38 +0100

    fixed problem

--- api/api_restaurantsFilter.php
+++ api/api_restaurantsFilter.php
@@ -4,7 +4,12 @@ require_once('../session.php');
 $session = new Session();
 require_once('../database/restaurant.class.php');
 $db = new PDO('sqlite:../example.db');
+if($_GET['filter']=="all"){
+    $restaurants=Restaurant::getRestaurants($db, $session);
+}
+else{
     $restaurants = Restaurant::filterRestaurants($db, $_GET['filter'], $session);
+}
 
 echo json_encode($restaurants);
 ?>
